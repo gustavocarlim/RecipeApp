85f9fc69b3be44827e4faa8e3e645405255425d0
gustavocarlim <gustavocarlimsaens@gmail.com>
2023-08-25 15:53:00 -0300

    requisitos maneiros

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import Login from './pages/Login/Login';
 import Meals from './pages/Meals/Meals';
 import Drinks from './pages/Drinks/Drinks';
 import Profile from './pages/Profile/Profile';
-import DoneRecipes from './pages/DoneRecipes/doneRecipies';
-import FavoriteRecipes from './pages/FavoriteRecipies/favoriteRecipies';
-import RecipiesProvider from './context/RecipiesProvider';
+import DoneRecipes from './pages/DoneRecipes/doneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes/favoriteRecipes';
+import RecipesProvider from './context/RecipesProvider';
 import RecipeDetails from './Components/RecipeDetails';
 
 function App() {

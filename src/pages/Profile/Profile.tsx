a21e197777a011a084cbd8d692f89e8e9d9c3c69
Alan Jorge <alan-jorge.1991@hotmail.com>
2023-08-25 22:47:02 -0300

    requisitos 57 a 59

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,6 +1,19 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Footer from '../../Components/Footer';
 
 function Profile() {
+  const [email, setEmail] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = localStorage.getItem('user');
+    if (user) {
+      const emailUser = JSON.parse(user);
+      setEmail(emailUser.email);
+    }
+  }, []);
+
   return (
     <>
       <div>
@@ -9,7 +22,27 @@ function Profile() {
           alt="Profile"
           data-testid="profile-top-btn"
         />
-        <h1 data-testid="page-title">Profile</h1>
+        <h1 data-testid="page-title">PROFILE</h1>
+        <h4
+          data-testid="profile-email"
+        >
+          {email}
+        </h4>
+        <button
+          data-testid="profile-done-btn"
+        >
+          Done Recipes
+        </button>
+        <button
+          data-testid="profile-favorite-btn"
+        >
+          Favorite Recipes
+        </button>
+        <button
+          data-testid="profile-logout-btn"
+        >
+          Logout
+        </button>
       </div>
       <div>
         <Footer />

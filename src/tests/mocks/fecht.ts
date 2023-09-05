46f0760377cf283d6e5399bab7d6f661a8c2b5cc
Fernanda <fernanda_petrachim@hotmail.com>
2023-09-05 13:03:09 -0300

    coreeções/19

--- src/tests/mocks/fecht.ts
+++ src/tests/mocks/fecht.ts
@@ -2,7 +2,7 @@ import mealCategories from './ mealCategories';
 import beefMeals from './beefMeals ';
 import breakfastMeals from './breakfastMeals';
 import chickenMeals from './chickenMeals';
-// import cocoaDrinks from './cocoaDrinks';
+import cocoaDrinks from './cocoaDrinks';
 import corba from './corba ';
 import dessertMeals from './dessertMeals';
 import drinksByFirstLetter from './drinksByFirstLetter';
@@ -17,10 +17,8 @@ import mealsByName from './mealsByName';
 import mealsList from './mealsList';
 import milkDrinks from './milkDrinks';
 import { drinkCategories } from './mockdrinkCategories';
-import cocktailDrinks from './ocktailDrinks';
 import oneDrinkId15997 from './ononeDrinkId15997';
 import ordinaryDrinks from './ordinaryDrinks';
-import otherDrinks from './otherDrinks';
 
 export const mockMealsFetch = (url: string) => Promise.resolve({
   ok: true,
@@ -46,8 +44,9 @@ export const mockMealsFetch = (url: string) => Promise.resolve({
     /*   if (url === 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Other/Unknown') return Promise.resolve(otherDrinks); */
     if (url === 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=15997') return Promise.resolve(oneDrinkId15997);
     if (url === 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=A') return Promise.resolve(drinksByFirstLetter);
-  /*   if (url === 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52977') return Promise.resolve(corba);
+    /*   if (url === 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52977') return Promise.resolve(corba);
     if (url === 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=15997') return Promise.resolve(gg); */
 
   },
 });
+//
